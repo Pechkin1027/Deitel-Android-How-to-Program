3c78108fb2902a647c063b26bf83a5dc0a7724cb
Pechkin1027 <benjamin.brusilovsky@gmail.com>
2022-05-07 16:56:49 -0400

    initial commit

---
+++ CannonGame/app/src/main/java/com/benjaminbrusilovsky/cannongame/MainActivity.java
@@ -0,0 +1,16 @@
+//“On my honor, I have neither received nor given any unauthorized assistance
+// on this assignment." -Benjamin Brusilovsky
+
+package com.benjaminbrusilovsky.cannongame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ FlagQuiz/app/src/main/java/com/benjaminbrusilovsky/flagquiz/QuizResult.java
@@ -0,0 +1,34 @@
+package com.benjaminbrusilovsky.flagquiz;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.fragment.app.DialogFragment;
+
+public class QuizResult extends DialogFragment {
+    public Dialog onCreateDialog(Bundle bundle) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        final MainActivityFragment quizFragment = getQuizFragment();
+
+        builder.setMessage(getString(R.string.results, quizFragment.totalGuesses, (1000 / (double) quizFragment.totalGuesses)));
+
+        builder.setPositiveButton(R.string.reset_quiz,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        quizFragment.resetQuiz();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    private MainActivityFragment getQuizFragment() {
+        MainActivityFragment quizFragment = MainActivity.result;
+        return quizFragment;
+    }
+
+
+}
